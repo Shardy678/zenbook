acddcdfc3a0739c985c48d26139ed374171239a3
Shardy678 <tambov.j678@gmail.com>
2021-11-19 15:25:55 +0300

    added AdSection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import BestSellerSection from "./components/BestSellerSection";
 import HeroSection from "./components/HeroSection";
+import AdSection from "./components/AdSection";
 
 function App() {
   return (
     <>
       <HeroSection />
       <BestSellerSection />
+      <AdSection />
     </>
   );
 }
